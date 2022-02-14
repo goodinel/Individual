0434ca18e5a5935716e3a7c653198a3235a3fa40
goodinel <goodinel@oregonstate.edu>
2022-02-13 22:52:55 -0800

    added sarcastic statement

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ async def getweather(usr_input):
     print("you air quality index = ",aqi)
     # returns the current day's forecast temperature (int)
     print("your current weather = ", weather.current.temperature)
+    print(statement(weather.current.temperature))
     # print("this is the type:" , type(weather.current.temperature))
 
     # get the weather forecast for a few days
@@ -35,6 +36,34 @@ async def getweather(usr_input):
 
     # close the wrapper once done
     await client.close()
+
+
+def statement(temp):
+    above80 = "Better know how to make a swamp air conditioner"
+    between79and70 = "Grab a beer 'cause its a little hot"
+    between50and69 = "Perfection... sunglasses and chill"
+    between49and32 = "Chilly but all is good"
+    between31and20 = "COLD"
+    between19and10 = "Really COLD"
+    between9and0   = "Yeah Really Really COLD"
+    belowzero      = "Stay home... alcohol is friend"
+
+    if temp > 80:
+        return above80
+    elif temp < 80 and temp >= 70:
+        return between79and70
+    elif temp < 70 and temp >= 50:
+        return between50and69
+    elif temp < 50 and temp >= 32:
+        return between49and32
+    elif temp < 32 and temp >= 20:
+        return between31and20
+    elif temp < 20 and temp >= 10:
+        return between19and10
+    elif temp < 10 and temp >= 0:
+        return between9and0
+    else:
+        return belowzero
 
 
 usr_input= input("city of interest? please enter here")
